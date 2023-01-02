1aef4e5bb7958dd6ba272917024bd8974fa52825
NaviteLogger5547 <66165957+NaviteLogger@users.noreply.github.com>
2023-01-02 12:08:09 +0100

    Added an hour variable which displays different greetings depending on the time of the day

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyFirstWebApp.Controllers
 {
     public class HomeController : Controller
     {
 
-        public ViewResult Index()
+        public ViewResult Index() //after the method is called, it returns the Index.cshtml view containing the "MyView" header and the string "Good morning"/"Good afternoon"/"Good evening"
         {
-            return View("MyView");
+            string greeting;
+            DateTime time = DateTime.Now;
+            int hour = time.Hour;
+            if (hour < 12)
+            {
+                greeting = "Good morning";
+            }
+            else if (hour < 18)
+            {
+                greeting = "Good afternoon";
+            }
+            else
+            {
+                greeting = "Good evening";
+            }
+            return View("MyView", greeting); //"MyView" is the name of the view file, and "greeting" is the model object passed to return
         }
     }
 }
